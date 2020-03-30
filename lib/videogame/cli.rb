788cf50aa7567b34b3fa534cdee17cae41208bfd
Charles Butler <cdbutle@outlook.com>
2020-03-30 00:30:29 +0000

    first commit

---
+++ lib/videogame/cli.rb
@@ -0,0 +1,8 @@
+require "videogame/cli/version"
+
+module Videogame
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/videogame/cli/version.rb
@@ -0,0 +1,5 @@
+module Videogame
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
